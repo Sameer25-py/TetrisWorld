c45810290a9b57e0c60e700791a1ce2c36b588ff
Muhammad Sameer <sameergb25@outlook.com>
2023-04-15 21:45:21 +0500

    import 2D assets and populate scriptable object

---
+++ Assets/Scripts/Letters.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace TetrisWorld
+{
+    [CreateAssetMenu(fileName = "Letters", menuName = "Letters", order = 0)]
+    public class Letters : ScriptableObject
+    {
+        public List<Sprite> LetterSprites;
+        public GameObject   LetterPrefab;
+    }
+}
