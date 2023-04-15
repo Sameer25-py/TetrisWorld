4cde201c5780fa37c39a3908a1bf817457818020
Muhammad Sameer <sameergb25@outlook.com>
2023-04-15 21:22:42 +0500

    populate scriptable object w/ words

---
+++ Assets/Scripts/DictionaryCreator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using UnityEngine;
+#if UNITY_EDITOR
+    using UnityEditor;
+#endif
+
+namespace TetrisWorld
+{
+    public class DictionaryCreator : MonoBehaviour
+    {
+        [SerializeField] private TextAsset Text;
+
+        private void Start()
+        {
+            CreateWordsDictionary(Text.text);
+        }
+
+        private void CreateWordsDictionary(string json)
+        {
+            WordsDict wordsDictionary = JsonConvert.DeserializeObject<WordsDict>(json);
+            var       newAsset        = ScriptableObject.CreateInstance<WordsDictionary>();
+            newAsset.Dictionary = new();
+            foreach (List<string> dictionary in wordsDictionary.Dictionary)
+            {
+                List<string> upperCaseDict = dictionary.Select(word => word.ToUpper())
+                    .ToList();
+                Words wordsList = new()
+                {
+                    WordsList = upperCaseDict
+                };
+                newAsset.Dictionary.Add(wordsList);
+            }
+#if UNITY_EDITOR
+            AssetDatabase.CreateAsset(newAsset, "Assets/WordsDictionary.asset");
+            AssetDatabase.SaveAssets();
+#endif
+        }
+    }
+}
+
+[Serializable]
+public class WordsDict
+{
+    public List<List<string>> Dictionary;
+}
---
+++ Assets/Scripts/WordsDictionary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WordsDictionary", menuName = "WordsDictionary", order = 0)]
+public class WordsDictionary : ScriptableObject
+{
+    public List<Words> Dictionary;
+}
+
+[Serializable]
+public class Words
+{
+    public List<string> WordsList;
+}
