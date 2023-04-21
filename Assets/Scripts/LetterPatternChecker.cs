8f39766bca6990995b3f5509e8acfc395525ae74
Muhammad Sameer <sameergb25@outlook.com>
2023-04-21 10:13:36 +0500

    add logic to generate word patterns from grid

---
+++ Assets/Scripts/LetterPatternChecker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace TetrisWorld
+{
+    public class LetterPatternChecker : MonoBehaviour
+    {
+        public string ValidPattern = "AAA";
+
+        public List<Pattern> GenerateCombinations(List<Letter> Letters)
+        {
+            List<Pattern> combinations = new();
+            for (int i = 0; i < Letters.Count; i++)
+            {
+                for (int j = i; j < Letters.Count; j++)
+                {
+                    Pattern pattern = new();
+                    for (int k = i; k <= j; k++)
+                    {
+                        pattern.Combination.Add(new Tuple<Letter, Index>
+                            (Letters[k], k));
+                    }
+
+                    if (pattern.Combination.Count is >= 3 and <= 7)
+                    {
+                        combinations.Add(pattern);
+                    }
+                }
+            }
+
+            return combinations;
+        }
+
+        public List<Pattern> MatchPattern(List<Letter> Letters)
+        {
+            return null;
+        }
+    }
+
+    [Serializable]
+    public class Pattern
+    {
+        public bool                       Matched     = false;
+        public List<Tuple<Letter, Index>> Combination = new();
+
+
+        public string getWordFromCombination()
+        {
+            return Combination.Aggregate("", (current, tuple) =>
+                current + tuple.Item1.PrefabLetter);
+        }
+    }
+}
--- Assets/Scripts/LetterPoolGenerator.cs
+++ Assets/Scripts/LetterPoolGenerator.cs
@@ -11,22 +11,40 @@ namespace TetrisWorld
         public List<Letter>          LetterPool;
         public Vector2               InitialPosition = new Vector2(-0.00999998301f, 3.6900003f);
         public LetterBlockController LetterBlockController;
+        public LetterPatternChecker  PatternChecker;
 
         private void GeneratePool()
         {
             LetterPool = new();
-            for (int i = 0; i < PoolLength; i++)
+            for (int i = 0; i < 26; i++)
             {
                 GameObject letterObj = Instantiate(Letters.LetterPrefab, InitialPosition, Quaternion.identity);
                 letterObj.SetActive(false);
                 Letter letter             = letterObj.GetComponent<Letter>();
-                Sprite randomLetterSprite = Letters.GetRandomLetterSprite();
+                Sprite randomLetterSprite = Letters.LetterSprites[i];
                 letter.PrefabLetter = randomLetterSprite.name;
-
                 letterObj.GetComponent<SpriteRenderer>()
                     .sprite = randomLetterSprite;
-
                 LetterPool.Add(letter);
+            }
+
+            List<Letter> letters = new();
+            for (int i = 0; i < 7; i++)
+            {
+                letters.Add(GetAvailableLetter());
+            }
+
+            var x = PatternChecker.GenerateCombinations(letters);
+            foreach (Pattern pattern in x)
+            {
+                string word = "";
+                foreach (var combination in pattern.Combination)
+                {
+                    word += (combination.Item2 + " " + combination.Item1.PrefabLetter + " ");
+                }
+                
+                Debug.Log(word);
+                word = "";
             }
         }
 
