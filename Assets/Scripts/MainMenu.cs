1075e69823563127f7b619221ec63023e0d1e05d
Muhammad Sameer <sameergb25@outlook.com>
2023-04-26 16:35:54 +0500

    implemented main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TetrisWorld
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public GameObject Menu, Settings;
+        public void PlayButton()
+        {
+            SceneManager.LoadScene("Gameplay", LoadSceneMode.Single);
+        }
+
+        public void SettingsButton()
+        {
+            Menu.SetActive(false);
+            Settings.SetActive(true);
+        }
+
+        public void BackToMenuButton()
+        {
+            Menu.SetActive(true);
+            Settings.SetActive(false);
+        }
+    }
+}
